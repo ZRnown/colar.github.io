122a6249972f6cbbcb60fa3f570e0f12bc8d2323
ZRnown <2902414706@qq.com>
2020-02-06 10:33:58 +0800

    Site updated: 2020-02-06 10:33:57

---
+++ js/main.js
@@ -0,0 +1,14 @@
+const app = new Vue({
+  el: ".posts",
+  data: {
+    // issbott 小动态下划线 istrans 背景颜色渐变 isbot 静态下划线
+    issbott: true,
+    istrans: true,
+    isbot: true,
+    ismouseleave: true,
+    isunder: true
+  },
+  methods: {
+
+  }
+})
